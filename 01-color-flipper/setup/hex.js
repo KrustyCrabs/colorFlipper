6077905851f100eab49b28cd6535ec8bfc059d99
KrustyCrabs <69775587+KrustyCrabs@users.noreply.github.com>
2022-11-24 13:09:55 +0530

    Add files via upload

---
+++ 01-color-flipper/setup/hex.js
@@ -0,0 +1,17 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    let hexValue = "#";
+    for(let i = 0; i < 6; i++){
+        hexValue += hex[generateRandomNumber()];
+    }
+    document.body.style.backgroundColor = hexValue;
+    color.textContent = hexValue;
+});
+
+function generateRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+}
